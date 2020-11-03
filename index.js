3a9b00fea72143a9793b11cd558976e9ea18e0b8
Ian Wren <ian.wren81@gmail.com>
2020-11-02 17:57:59 -0800

    Added inquirer with inputs that uses the questions array.

--- index.js
+++ index.js
@@ -1,12 +1,71 @@
+var inquirer = require("inquirer");
+
 // array of questions for user
-const questions = ["What is your gitHub user name?", "What is your email address?",  "Please describe your project:","Please describe installation procedures:",
-"Please enter usage instructions:", "Any contribution guidelines?", "Please list any test instructions"
+const questions = ["What is your gitHub user name?", "What is your email address?", "What is the title of your project?" "Please describe your project:", "Please describe installation procedures:",
+    "Please enter usage instructions:", "Any contribution guidelines?", "Please list any test instructions"
 
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: questions[0],
+            name: "username"
+        },
+        {
+            type: "input",
+            message: questions[1],
+            name: "email"
+        },
+
+        {
+            type: "input",
+            message: questions[2],
+            name: "title"
+        },
+
+
+        {
+            type: "input",
+            message: questions[3],
+            name: "description"
+        },
+
+        {
+            type: "input",
+            message: questions[4],
+            name: "install"
+        },
+
+        {
+            type: "input",
+            message: questions[5],
+            name: "usage"
+        },
+
+        {
+            type: "input",
+            message: questions[6],
+            name: "contribution"
+        },
+
+        {
+            type: "input",
+            message: questions[7],
+            name: "test"
+        },
+    ]).then(function (response) {
+
+
+        // function to write README file
+        function writeToFile(fileName, data) {
+        }
+    });
+
+
 
 // function to initialize program
 function init() {
