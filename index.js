152e3d9019d3c1a1afee7e63987d6bcd12d86312
Ian Wren <ian.wren81@gmail.com>
2020-11-06 18:57:28 -0800

    Created function to add the license badge to the created file and changed code to take data and stage it to create the new file.

--- index.js
+++ index.js
@@ -1,104 +1,128 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
+let generateMD = require("./generateMardown.js")
 
 // array of questions for user
 const questions = ["What is your gitHub user name?", "What is your email address?", "What is the title of your project?", "Please describe your project:", "Please describe installation procedures:",
     "Please enter usage instructions:", "Any contribution guidelines?", "Please list any test instructions", "Please choose a license"
 
 ];
-
-
-
-inquirer
-    .prompt([
-        {
-            type: "input",
-            message: questions[0],
-            name: "username"
-        },
-        {
-            type: "input",
-            message: questions[1],
-            name: "email"
-        },
-
-        {
-            type: "input",
-            message: questions[2],
-            name: "title"
-        },
-
-
-        {
-            type: "input",
-            message: questions[3],
-            name: "description"
-        },
-
-        {
-            type: "input",
-            message: questions[4],
-            name: "install"
-        },
-
-        {
-            type: "input",
-            message: questions[5],
-            name: "usage"
-        },
-
-        {
-            type: "input",
-            message: questions[6],
-            name: "contribution"
-        },
-
-        {
-            type: "input",
-            message: questions[7],
-            name: "test"
-        },
-        
-        {
-            type: "checkbox",
-            message: questions[8],
-            name: "license",
-            choices: [
-              "MIT", 
-              "GNU GPLv3", 
-              "Mozilla PL 2.0", 
-              "Apache 2.0",
-              "Boost Software 1.0",
-              "Unlicense/Other"
-
-            ]
-          }
-    ]).then(function(data) {
-        // var readMedata = data.name.toLowerCase().split(' ').join('') + ".json";
-
-        // fs.writeFile(readMedata, JSON.stringify(data, null, '\t'), function(err) {
-      
-        //   if (err) {
-        //     return console.log(err);
-        //   }
-      
-        //   console.log("Success!");
-      
-
-
-        // function to write README file
-       
-    function writeToFile(response, data) {
-    }
-
-    });
-
-
-
 // function to initialize program
 function init() {
 
-}
+
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: questions[0],
+                name: "username"
+            },
+            {
+                type: "input",
+                message: questions[1],
+                name: "email"
+            },
+
+            {
+                type: "input",
+                message: questions[2],
+                name: "title"
+            },
+
+
+            {
+                type: "input",
+                message: questions[3],
+                name: "description"
+            },
+
+            {
+                type: "input",
+                message: questions[4],
+                name: "install"
+            },
+
+            {
+                type: "input",
+                message: questions[5],
+                name: "usage"
+            },
+
+            {
+                type: "input",
+                message: questions[6],
+                name: "contribution"
+            },
+
+            {
+                type: "input",
+                message: questions[7],
+                name: "test"
+            },
+
+            {
+                type: "list",
+                message: questions[8],
+                name: "license",
+                choices: [
+                    "MIT",
+                    "GNU GPLv3",
+                    "Mozilla PL 2.0",
+                    "Apache 2.0",
+                    "Boost Software 1.0",
+                    "Unlicense/Other"
+
+                ]
+            }
+        ]).then(function (data) {
+            console.log(data);
+            let userdata = { ...data };
+            userdata.licenseBadge = "";
+
+            data.license.map(userlicense => {
+                if (userlicense === "MIT") {
+                    userdata.licenseBadge = "https://img.shields.io/badge/License-MIT-yellow.svg";
+                }
+                else if (license === "GNU GPLv3") {
+                    userdata.licenseBadge = "https://img.shields.io/badge/License-GPLv3-blue.svg";
+                }
+                else if (license === "Mozilla PL 2.0") {
+                    userdata.licenseBadge = "https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg";
+                }
+                else if (license === "Apache 2.0") {
+                    userdata.licenseBadge = "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+                }
+                else if (license === "Boost Software 1.0") {
+                    userdata.licenseBadge = "https://img.shields.io/badge/License-Boost%201.0-lightblue.svg";
+                }
+                else if (license === "Unlicense/Other") {
+                    userdata.licenseBadge = "https://img.shields.io/badge/license-Unlicense-blue.svg";
+                }
+
+            })
+
+
+            fs.writeFile("README.md", generateMD(userdata), function (err) {
+
+                if (err) {
+
+                    return console.log(err);
+                }
+
+                console.log("Success!");
+
+
+
+
+
+            });
+
+
+        });
+
+
+};
 
 // function call to initialize program
 init();
