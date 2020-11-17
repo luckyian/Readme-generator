26c2029d8fc27261cb072549c2031a284587b1cc
Ian Wren <ian.wren81@gmail.com>
2020-11-16 16:58:11 -0800

    Added functionality to function that sets and displays the license badge at the top of the readme page.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
-let generateMD = require("./generateMardown.js")
+let generateMD = require("./generateMarkdown.js")
 
 // array of questions for user
 const questions = ["What is your gitHub user name?", "What is your email address?", "What is the title of your project?", "Please describe your project:", "Please describe installation procedures:",
     "Please enter usage instructions:", "Any contribution guidelines?", "Please list any test instructions", "Please choose a license"
 
 ];
+
+
 // function to initialize program
 function init() {
+
+
 
 
     inquirer
@@ -76,16 +80,22 @@ function init() {
                 ]
             }
         ]).then(function (data) {
+            
             console.log(data);
             let userdata = { ...data };
             userdata.licenseBadge = "";
+            console.log(userdata);
+            
+            console.log(userdata.license);
 
-            data.license.map(userlicense => {
-                if (userlicense === "MIT") {
-                    userdata.licenseBadge = "https://img.shields.io/badge/License-MIT-yellow.svg";
+            
+           
+            function licenseIcon(license) { 
+                if (license === "MIT") {
+                   return userdata.licenseBadge = "https://img.shields.io/badge/License-MIT-yellow.svg";
                 }
                 else if (license === "GNU GPLv3") {
-                    userdata.licenseBadge = "https://img.shields.io/badge/License-GPLv3-blue.svg";
+                   return userdata.licenseBadge = "https://img.shields.io/badge/License-GPLv3-blue.svg";
                 }
                 else if (license === "Mozilla PL 2.0") {
                     userdata.licenseBadge = "https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg";
@@ -99,8 +109,13 @@ function init() {
                 else if (license === "Unlicense/Other") {
                     userdata.licenseBadge = "https://img.shields.io/badge/license-Unlicense-blue.svg";
                 }
+            
+            }
+           
+            licenseIcon(userdata.license);
+            
 
-            })
+            
 
 
             fs.writeFile("README.md", generateMD(userdata), function (err) {
